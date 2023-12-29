0d0d5f3ad067053164df69d0617bc1774112e005
developertool45 <developertool45@gmail.com>
2023-12-29 18:30:01 +0530

    change root

--- reatapp/src/index.js
+++ reatapp/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ ReactDOM.createRoot(document.getElementById('root'))
+.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
