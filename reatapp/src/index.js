bb8f42ddd9af5079e6d979e545c971089336f34e
developertool45 <developertool45@gmail.com>
2023-12-30 09:40:49 +0530

    index.js

--- reatapp/src/index.js
+++ reatapp/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 
 
- ReactDOM.createRoot(document.getElementById('root'))
-.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
