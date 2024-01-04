97392344f541126ba428718f816264df2494b115
developertool45 <developertool45@gmail.com>
2024-01-04 13:04:34 +0530

    add all

---
+++ counter/counter/src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+import { useState } from 'react';
+
+
+
+
+function App() { 
+
+  let [counter, setCounter] = useState(15);
+  
+  const addvalue = () =>{  
+    
+      if( counter <20 ){
+     counter = counter+1;
+      setCounter(counter );
+    }
+    
+  }
+  const decvalue = () =>{
+    if( counter > 0 ){
+      counter = counter-1;
+       setCounter(counter );
+     }
+      
+  }
+
+  return (    
+    <>
+       <h1> counter value is  : {counter}</h1>
+       <button onClick= {addvalue}>increase  : {counter}</button>
+       <button onClick= {decvalue} >decrease  : {counter}</button>
+       <p>value is : {counter}</p>
+       
+      
+    </>
+  )
+}
+
+export default App
